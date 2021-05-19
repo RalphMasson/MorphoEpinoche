f388cd9587bd98b8b1e88a25fe5e34a6da722010
Ralph Masson <ralph.masson@gmail.com>
2021-05-19 11:45:24 +0200

    update class diagram

--- script/py2diagram.ps1
+++ script/py2diagram.ps1
@@ -1,8 +1,8 @@
 cd ..
 cd code
 echo "Conversion du code en graphe"
-Pyreverse -o dot -p Interface Interface_Sexage_Normal.py
-Pyreverse -o dot -p Interface Interface_Sexage_Little.py
+Pyreverse -o dot -p Interface_Normal Interface_Sexage_Normal.py
+Pyreverse -o dot -p Interface_Little Interface_Sexage_Little.py
 Pyreverse -o dot -p Placement Placement.py
 Pyreverse -o dot -p Classification Classification.py
 Pyreverse -o dot -p Fonctions Fonctions.py
