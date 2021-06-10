29f3b09520864fdf20b64ec8e080260c0742a1bd
Ralph Masson <ralph.masson@gmail.com>
2021-06-10 18:21:14 +0200

    prepare classification

--- code/Classification.py
+++ code/Classification.py
@@ -1,4 +1,23 @@
 class Prediction():
+    """!
+        Classe de prediction du sexe du poisson par Machine Learning
+        Nécessite d'avoir un modèle de prédiction des points et du sexe avec un image
+
+        Adapted from :
+            Kazemi,Sullivan, "One millisecond face alignment with an ensemble of regression trees," doi: 10.1109/CVPR.2014.241.       2014
+            Perrot,Bourdon,Helbert "Implementing cascaded regression tree-based face landmarking" doi: 10.1016/j.imavis.2020.103976   2020
+            Porto, Voje "ML-morph: [...] automated [...] landmarking of biological structures in images" 10.1111/2041-210X.13373      2020
+            Irani, Allada.. "Highly versatile facial landmarks detection models using ensemble of regression trees with application"  2019
+    """
+    def preprocess():
+        return 0
+
+    def train():
+        return 0
+
+    def test():
+        return 0
+
     def predict():
         from random import randrange,uniform
         labels = ('Male','Female')
