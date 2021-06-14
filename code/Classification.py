a0b5b3d7d8dc31f398639e89a43ca185f851511f
Ralph Masson <ralph.masson@gmail.com>
2021-06-14 11:02:36 +0200

    update classification preprocessing and training

--- code/Classification.py
+++ code/Classification.py
@@ -1,3 +1,12 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+import pandas as pd
+
 class Prediction():
     """!
         Classe de prediction du sexe du poisson par Machine Learning
@@ -9,14 +18,44 @@ class Prediction():
             Porto, Voje "ML-morph: [...] automated [...] landmarking of biological structures in images" 10.1111/2041-210X.13373      2020
             Irani, Allada.. "Highly versatile facial landmarks detection models using ensemble of regression trees with application"  2019
     """
-    def preprocess():
-        return 0
 
-    def train():
-        return 0
 
-    def test():
-        return 0
+    def __init__(self,bdd):
+        """!
+            Constructeur du prédicteur de sexe
+            @param bdd chemin du fichier csv (default = "C:\\Users\\MASSON\\Desktop\\STAGE_EPINOCHE\\moduleMorpho\\rapports\\"
+        """
+        self.bdd = pd.read_csv(bdd,delimiter=";")
+
+
+    def preprocess(self):
+        """!
+            Récupère la base de données complète avec les distances, angles et sexe
+            @param df dataframe
+            @return Xtrain Xtest,yTrain yTest
+        """
+
+        self.X = self.bdd.drop('Sexe (0:F, 1:M)',axis=1)
+        self.y = self.bdd[self.bdd.columns[0]]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.3)
+
+
+    def train(self):
+        """!
+            Train with RF & SVC
+        """
+
+        self.clf=RandomForestClassifier(n_estimators=200)
+        self.clf.fit(self.X_train,self.y_train)
+        print(self.clf.score(self.X_train,self.y_train))
+        print(self.clf.score(self.X_test,self.y_test))
+
+        self.clf1 = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+        self.clf1.fit(self.X_train,self.y_train)
+        print(self.clf1.score(self.X_train,self.y_train))
+        print(self.clf1.score(self.X_test,self.y_test))
+
+
 
     def predict():
         from random import randrange,uniform
